06eb29be8f90290d9d842c2e2cf6d299971616ab
Spirit Payton <98915494+SpiritPayton@users.noreply.github.com>
2022-03-07 12:40:29 +1300

    ros

--- Exercise 9.py
+++ Exercise 9.py
@@ -1,17 +1,21 @@
-# Function to print longest wod in given list
+# Function to print longest word(s) in a list
 
 
 def longest_word(word_list):
     longest = ""
     for word in word_list:
-        if len(word) > len(longest):
-            longest = word
+        if len(word) > len(longest[0]):
+            longest = [word]
+        elif len(word) == len(longest[0]):
+            longest.append(word)
     return longest
 
 
 # Main routine
-words = {}
+words = []
 word_ = ""
 while word_ != "1":
-    word_ = input("Add word to list")
-    
+    word_ = input("Add word to list (or 1 to end): ")
+    words.append(word_)
+
+print (f"the longest word/words in the list {words} is {longest_word()}")
